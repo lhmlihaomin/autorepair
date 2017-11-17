004c2020c6a0e2e1010a587a0f657630af90465d
Li Haomin <lhmlihaomin@live.com>
2017-11-17 09:16:52 +0800

    Added static resource files

--- web/asset/urls.py
+++ web/asset/urls.py
@@ -2,14 +2,7 @@ from django.conf.urls import url
 from asset import views
 
 urlpatterns = [
-    url(r'sync_ec2', views.sync_ec2),
-    url(r'api/$', views.OnlineEventList.as_view()),
-    url(r'api/(?P<pk>[0-9]+)/$', views.OnlineEventDetail.as_view()),
-    url(r'api/create_event/$', views.xxx),
-    #url(r'api/modules/(?P<region_name>.+)/(?P<module_name>.+)/(?P<version>.+)/$', views.xxx),
-    ## Use the same url for PATCH: url(r'api/modules/(?P<region_name>.+)/(?P<module_name>.+)/(?P<version>.+)/$', views.xxx),
-    #url(r'api/create_ec2_instances/$', views.xxx),
-    #url(r'api/deactivate_ec2_instances/$', views.xxx),
-    #url(r'api/pause_auto_repair/$', views.xxx),
-    #url(r'api/resume_auto_repair/$', views.xxx),
+    url(r'test/$', views.test),
+
+    url(r'online_events/$', views.OnlineEventsView.as_view()),
 ]
--- web/asset/views.py
+++ web/asset/views.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+from asset.models import OnlineEvent
+
+
+def test(request):
+    return render(request, 'asset/test.html')
+
+
+class OnlineEventsView(View):
+    template_name = 'asset/online_events.html'
+    context = {
+        'title': "Online Events"
+    }
+
+    def get(self, request):
+        events = OnlineEvent.objects.all()
+        self.context.update({
+            'events': events
+        })
+        return render(request, self.template_name, self.context)
+
+
+class EventLogView():
+    pass
+
+
+class EC2InstanceView():
+    pass
